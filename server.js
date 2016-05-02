db648b172c29bb4aa356bd71b858b3d720fbd2d5
Dan Kaiser <dskaiser82@gmail.com>
2016-05-02 14:39:47 -0700

    changing state change on else

--- server.js
+++ server.js
@@ -195,24 +195,16 @@ bot.onTextMessage((message) => {
 
 	////================================================================================================
 
-		// else if(message.body === "Too busy.  Sorry." || message.body === "No F-ing way"){//Leave Message
-		// 	user.state = user.state+1
-		// 	outgoingMessage = Bot.Message.text("Cool.  TTYL")
-	  //   outgoingMessage.addResponseKeyboard(["BRB"], false, message.from)}
-
-		else if(messages.picUrl === true){ //for first message
-			outgoingMessage = Bot.Message.text(exclaims[mathRan()])
-					outgoingMessage.addResponseKeyboard(["Keep Going"], false, message.from)
-					user.state = user.state+1
-					console.log(user.state, "This is the else")
-			}
+		else if(message.body === "Keep Going"){//Leave Message
+			user.state = user.state+1
+		}
 
 
 
 		else{ //for first message
 			outgoingMessage = Bot.Message.text(exclaims[mathRan()])
 			    outgoingMessage.addResponseKeyboard(["Keep Going"], false, message.from)
-					user.state = user.state+1
+					// user.state = user.state+1
 					console.log(user.state, "This is the else")
 	  	}
 			///Ivar emoji test
